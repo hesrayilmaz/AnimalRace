112e830c5e58e8435c398a45dcb28ccec751e155
hesrayilmaz <hesra.yilmazz@gmail.com>
2023-05-23 21:48:39 +0300

    control character with joystick and make camera follow the character

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 offset;
+    private Vector3 newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        newPosition = new Vector3(offset.x + target.position.x, offset.y + target.position.y, offset.z + target.position.z);
+        //transform.position = Vector3.Lerp(transform.position, newPosition, 10 * Time.deltaTime);
+        transform.position = newPosition;
+        //transform.rotation = target.rotation;
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private FixedJoystick fixedJoystick;
+    Vector3 direction, addedPos;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float forwardSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fixedJoystick.Vertical != 0 || fixedJoystick.Horizontal != 0)
+        {
+            //Debug.Log("1111");
+            WalkAnimation();
+        }
+        else
+        {
+            IdleAnimation();
+        }
+    }
+
+    private void IdleAnimation()
+    {
+        animator.SetTrigger("Idle");
+    }
+
+    private void WalkAnimation()
+    {
+        animator.SetTrigger("Walk");
+    }
+
+    private void FixedUpdate()
+    {
+        if (fixedJoystick.Vertical != 0 || fixedJoystick.Horizontal != 0)
+        {
+            addedPos = new Vector3(fixedJoystick.Horizontal * forwardSpeed * Time.fixedDeltaTime, 0, fixedJoystick.Vertical * forwardSpeed * Time.fixedDeltaTime);
+            transform.position += addedPos;
+
+            direction = (Vector3.forward * fixedJoystick.Vertical + Vector3.right * fixedJoystick.Horizontal);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), Time.fixedDeltaTime * rotateSpeed);
+
+        }
+    }
+}
