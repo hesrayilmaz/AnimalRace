177e279c6d27e6bf70f5c0f169d11f658942c227
Uğur Kellecioğlu <ugur.kellecioglu@outlook.com>
2023-05-28 02:00:19 +0300

    add shop button to main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,18 @@ using Photon.Pun;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene("Loading");
     }
 
-    private void OnApplicationQuit()
+    public void LoadSelectedScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /*private void OnApplicationQuit()
     {
         PhotonNetwork.LeaveRoom();
-    }
+    }*/
 }
