99d82002899a7bb228247df88297c6dfb04896b3
Uğur Kellecioğlu <ugur.kellecioglu@outlook.com>
2023-05-30 14:43:47 +0300

    create level1 design
    fix spawned obstacle rotations

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -9,13 +9,13 @@ public class ObjectSpawner : MonoBehaviour
     [SerializeField] private GameObject obstaclePrefab;
     [SerializeField] private Transform objectContainer;
     [SerializeField] private PathCreator pathCreator;
-    [SerializeField] private float obstacleSpawnSpeed = 2f;
-    [SerializeField] private float distanceBetweenObstacles = 10f;
+    private float distanceBetweenObjects = 60f;
+    private float distanceBetweenObstacles = 120f;
 
     private Vector3[] spawnPositions;
-    private float currentDistance = 0f;
     private float totalPathLength;
-    private float spawnPoint = 10f;
+    private float spawnObjectPoint = 50f;
+    private float spawnObstaclePoint = 100f;
     private float spacing = 2.5f; // Distance between each spawned object
 
     private void Start()
@@ -23,11 +23,16 @@ public class ObjectSpawner : MonoBehaviour
         totalPathLength = pathCreator.path.length;
         spawnPositions = new Vector3[4];
         Debug.Log("totalPathLength: " + totalPathLength);
+
         if (PhotonNetwork.IsMasterClient)
         {
-            while (spawnPoint <= totalPathLength)
+            while (spawnObjectPoint <= totalPathLength)
             {
                 SpawnObject();
+            }
+
+            while (spawnObstaclePoint <= totalPathLength)
+            {
                 SpawnObstacle();
             }
         }
@@ -38,14 +43,14 @@ public class ObjectSpawner : MonoBehaviour
         // Select a random obstacle prefab from the available options
         GameObject objectPrefab = objectPrefabs[Random.Range(0, objectPrefabs.Length)];
 
-        Vector3 position = pathCreator.path.GetPointAtDistance(spawnPoint);
+        Vector3 position = pathCreator.path.GetPointAtDistance(spawnObjectPoint);
 
         // Calculate the normalized tangent and normal vectors of the path
-        Vector3 tangent = pathCreator.path.GetDirectionAtDistance(spawnPoint);
+        Vector3 tangent = pathCreator.path.GetDirectionAtDistance(spawnObjectPoint);
         Vector3 normal = new Vector3(-tangent.z, 0f, tangent.x).normalized;
 
-        // Calculate the rotation of the object based on the tangent
-        //Quaternion rotation = Quaternion.LookRotation(tangent, Vector3.up);
+        // Calculate the rotation of the object based on the normal
+        Quaternion rotation = Quaternion.LookRotation(normal, Vector3.up);
 
         // Offset the initial object's position based on the normal vector
         position += -normal * spacing * 1.5f;
@@ -53,19 +58,19 @@ public class ObjectSpawner : MonoBehaviour
         // Spawn the rest of the objects with spacing and rotation
         for (int i = 0; i < 4; i++)
         {
-            GameObject spawnedObj = PhotonNetwork.Instantiate(objectPrefab.name, new Vector3(position.x,objectPrefab.transform.position.y,position.z), Quaternion.identity);
+            GameObject spawnedObj = PhotonNetwork.Instantiate(objectPrefab.name, new Vector3(position.x,objectPrefab.transform.position.y,position.z), rotation);
             position += normal * spacing; // Add spacing in the direction of the normal vector
         }
 
-        spawnPoint += spawnPoint;
+        spawnObjectPoint += distanceBetweenObjects;
     }
 
     private void SpawnObstacle()
     {
-        Vector3 position = pathCreator.path.GetPointAtDistance(spawnPoint);
+        Vector3 position = pathCreator.path.GetPointAtDistance(spawnObstaclePoint);
 
         // Calculate the normalized tangent and normal vectors of the path
-        Vector3 tangent = pathCreator.path.GetDirectionAtDistance(spawnPoint*2);
+        Vector3 tangent = pathCreator.path.GetDirectionAtDistance(spawnObstaclePoint);
         Vector3 normal = new Vector3(-tangent.z, 0f, tangent.x).normalized;
 
         // Offset the initial object's position based on the normal vector
@@ -82,12 +87,12 @@ public class ObjectSpawner : MonoBehaviour
         for(int i=0; i<randomObstacleCount; i++)
         {
             int randomPosition = Random.Range(0, spawnPositions.Length);
-            Quaternion rotation = Quaternion.LookRotation(tangent, Vector3.up);
+            Quaternion rotation = Quaternion.LookRotation(normal, Vector3.up);
             GameObject spawnedObj = PhotonNetwork.Instantiate(obstaclePrefab.name, 
                 new Vector3(spawnPositions[randomPosition].x, obstaclePrefab.transform.position.y, spawnPositions[randomPosition].z),
                 rotation);
         }
 
-        spawnPoint += spawnPoint*2;
+        spawnObstaclePoint += distanceBetweenObstacles;
     }
 }
